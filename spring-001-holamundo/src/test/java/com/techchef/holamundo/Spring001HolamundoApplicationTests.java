27201970cfbbc82ee1ec8f40ab989e56517867ca
JehielL <jehiel.py@gmail.com>
2024-01-23 22:48:47 +0100

    Initial commit maven

---
+++ spring-001-holamundo/src/test/java/com/techchef/holamundo/Spring001HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchef.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring001HolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
