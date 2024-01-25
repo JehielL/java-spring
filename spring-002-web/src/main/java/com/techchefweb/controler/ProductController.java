521ca5a25b6c3dd729b4c020d514d03ebc9c6408
JehielL <jehiel.py@gmail.com>
2024-01-25 19:07:06 +0100

    Spring method @PostMapping @RequestBody

--- spring-002-web/src/main/java/com/techchefweb/controler/ProductController.java
+++ spring-002-web/src/main/java/com/techchefweb/controler/ProductController.java
@@ -2,9 +2,7 @@ package com.techchefweb.controler;
 
 import com.techchefweb.model.Product;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -71,25 +69,26 @@ public class ProductController {
         return new Product(1L, title, 399.0, true, LocalDate.now());
 
     }
-
     @GetMapping("products/by-price/min/{min}/max/{max}")
     public List<Product> findProductByprice(@PathVariable Double min, @PathVariable Double max){
-
 
         return List.of(
                 new Product(1L, "Tv Samsung 52", 400.0, true, LocalDate.now()),
                 new Product(2L, "PC MSI modern", 600.0, false, LocalDate.now()),
                 new Product(3L, "One plus 12", 900.0, false, null),
-                new Product(3L, "Android", 100.0, false, null)
+                new Product(4L, "Android", 100.0, false, null)
         );
-
-
-
     }
 
+    @PostMapping("products")
 
+    public Product create(@RequestBody Product product){
 
+        System.out.println(product);
 
+        return product;
+
+    }
 
 }
 
