d0aa34ba4bf9624b8a5bb54e61ca6bebba3d4e08
JehielL <jehiel.py@gmail.com>
2024-01-30 17:55:46 +0100

    Spring Methods Crud

--- spring-003-web-devtools/src/main/java/com/techchef/controller/BookController.java
+++ spring-003-web-devtools/src/main/java/com/techchef/controller/BookController.java
@@ -1,11 +1,8 @@
 package com.techchef.controller;
-
 import com.techchef.model.Author;
 import com.techchef.model.Book;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +11,10 @@ import java.util.List;
 public class BookController {
 
     //declarar array list de books
-   List<Book> books;
+    List<Book> books;
 
     //constructor
-    public BookController(){
+    public BookController() {
         books = new ArrayList<>();
         books.add(
                 new Book(1L,
@@ -25,7 +22,7 @@ public class BookController {
                         10.50,
                         true,
                         new Author(1L, "Carmen mola", "Madrid"))
-                );
+        );
         books.add(
                 new Book(2L,
                         "Es Peligroso asomarse al exterior",
@@ -44,15 +41,15 @@ public class BookController {
     }
 
     @GetMapping("books")
-    public ResponseEntity<List<Book>> findAll(){
+    public ResponseEntity<List<Book>> findAll() {
         return ResponseEntity.ok(this.books);
     }
 
     @GetMapping("books/{id}")
-    public ResponseEntity<Book> findById(@PathVariable Long id){
-        for(Book book:this.books){
+    public ResponseEntity<Book> findById(@PathVariable Long id) {
+        for (Book book : this.books) {
 
-            if (book.id().equals(id)){
+            if (book.id().equals(id)) {
 
                 return ResponseEntity.ok(book);
             }
@@ -61,6 +58,104 @@ public class BookController {
         return ResponseEntity.notFound().build();
     }
 
+    @GetMapping("books/by-author-id/{authorId}")
+    public ResponseEntity<List<Book>> findAllByAuthorId(@PathVariable Long authorId) {
+
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : this.books) {
+            if (book.author().id().equals(authorId)) {
+                filteredBooks.add(book);
+            }
+
+        }
+
+        if (filteredBooks.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(filteredBooks);
+
+    }
+
+    @PostMapping("books")
+    public ResponseEntity<Book> create(@RequestBody Book book) {
+
+        if (book.id() != null)
+            return ResponseEntity.badRequest().build();
+
+        this.books.add(book);
+
+        return ResponseEntity.status(200).body(book);
+
+    }
+
+    @PutMapping("books/{id}")
+    public ResponseEntity<Book> update(@RequestBody Book book, @PathVariable Long id) {
+
+        if (id == null || book.id() == null)
+            return ResponseEntity.badRequest().build();
+        // opcion 1
+        int index = -1;
+        for (Book bookFromList : this.books) {
+            if (bookFromList.id().equals(id)) {
+                index = this.books.indexOf(bookFromList);
+            }
+        }
+
+        for (int i = 0; i < this.books.size(); i++) {
+            if (this.books.get(i).id().equals(id)) {
+                index = i;
+            }
+
+
+        }
+        //Comprobar si se ha encontrado el libro
+
+        if (index == -1)
+            return ResponseEntity.notFound().build();
+
+        //
+
+        this.books.set(index, book);
+        return ResponseEntity.ok(book);
+
+
+
+
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+
+        int index = -1;
+        for(Book bookFromList: this.books){
+            if (bookFromList.id().equals(id)){
+                index = this.books.indexOf(bookFromList);
+
+            }
+        }
+
+        if(index == -1)
+            return ResponseEntity.badRequest().build();
+
+
+        this.books.remove(index);
+
+
+        return ResponseEntity.noContent().build();
+
+
+
+    }
+
+    @DeleteMapping("books")
+    public ResponseEntity<Void> deleteAll(){
+
+        this.books.clear();
+
+        return ResponseEntity.noContent().build();
+
+
+    }
 
 
 }
