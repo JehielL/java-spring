4dc8e38117716da0577b851292a1f87bd3ea5a30
JehielL <jehiel.py@gmail.com>
2024-02-01 18:59:48 +0100

    Creados clases Books, Author

---
+++ spring-005-web-jackson-ignore/src/main/java/com/techchef/models/Author.java
@@ -0,0 +1,17 @@
+package com.techchef.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Author {
+    private Long id;
+    private String name;
+    private String Biography;
+    private LocalDate startCarrer;
+    private List<Book> books;
+
+    //asociacion
+
+
+
+}
---
+++ spring-005-web-jackson-ignore/src/main/java/com/techchef/models/Book.java
@@ -0,0 +1,11 @@
+package com.techchef.models;
+
+public class Book {
+    private Long id;
+    private String title;
+    private Double price;
+    private Integer numPages;
+    private Boolean available;
+    private Author author;
+
+}
